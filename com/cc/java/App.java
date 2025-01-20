b8caefbac97f74d6ad6b23218a2d48945fc3a55c
CCHobbit <Markus_Clephas@t-online.de>
2025-01-20 12:44:44 +0100

    App_konto1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,14 +1,57 @@
 package com.cc.java;
 
+// Klassen Konto
+class Konto {
+
+    int kontostand = 10000;
+
+    // Methode zur Verdopplung des Kontostands
+    public void verdoppeln(){
+        kontostand *= 2;
+    }
+
+    // Methode zur Verdreifachung des Kontostands
+    public void verdreifachen(){
+        kontostand *= 3;
+    }
+
+    // Methode zur Verzehnfachung des Kontostands
+    public void verzehnfachen(){
+        kontostand *= 10;
+    }
+
+    // Methode zur Ausgabe des Kontostands
+    public void ausgeben(){
+        System.out.println("Kontostand: " + kontostand);
+    }
+}
+
 public class App {
     
     public static void main(String[] args) {
-    
+        // Erstellen von 3 Konto-Objekten
+        Konto konto1 = new Konto();
+        Konto konto2 = new Konto();
+        Konto konto3 = new Konto();
+
+        // Ausgabe des Anfangs-Kontostands
+        System.out.println("Anfangs-Kontostände:");
+        konto1.ausgeben();
+        konto2.ausgeben();
+        konto3.ausgeben();
+
+        // Änderung des Kontostands
+        konto1.verdoppeln();
+        konto2.verdreifachen();
+        konto3.verzehnfachen();
+
+        // Ausgabe des geänderten Kontostands
+        System.out.println("\nGeänderte Kontostände:");
     }
 
-
-    private static void output(String outputStr) {
-        System.out.println(outputStr);
-    } 
+    // private static void output(String outputStr) {
+    //     System.out.println(outputStr);
+    // } 
 
 }
+   
